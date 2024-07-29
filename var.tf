9d47a420c5e65787462b3ced486b878c5b4c739c
prashuchauhan1 <prashu.chauhan@hotmail.com>
2024-07-30 00:29:48 +0530

    first

---
+++ var.tf
@@ -0,0 +1,13 @@
+.,module d sd',
+data tls_public_key name {
+  private_key_pem = ""
+}
+ {
+  source  = "owner/d sd',
+  data tls_public_key name {
+    private_key_pem = ""
+  }
+  /acme"
+  version = "version"
+  param   = value
+}
